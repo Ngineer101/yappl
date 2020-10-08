de585ef83303c3c397f17376dc7afa419319c0f2
Ngineer101 <nicolaasb@pm.me>
2020-10-08 22:11:27 +0200

    Added JWT configuration and check if user is signed in

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -59,7 +59,19 @@ const options = {
       },
     }
   }),
-  // TODO: Add config settings for JWT authentication
+  secret: process.env.APP_SECRET,
+  session: {
+    jwt: true, // (Use JSON Web Tokens for session instead of database sessions)
+    maxAge: 7 * 24 * 60 * 60, // 30 days (Seconds - How long until an idle session expires and is no longer valid)
+    updateAge: 24 * 60 * 60, // 24 hours (Seconds - Throttle how frequently to write to database to extend a session)
+  },
+  jwt: {
+    // A secret to use for key generation - you should set this explicitly
+    // Defaults to NextAuth.js secret if not explicitly specified.
+    secret: process.env.JWT_SECRET,
+    encryption: true,
+  },
+  debug: true, // TODO: Change to false
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<any>) => NextAuth(req, res, options)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
 import Nav from '../components/nav'
 
 export default function IndexPage() {
+  const [session, loading] = useSession()
   return (
     <div>
       <Nav />
+      {!session && <>
+        Not signed in <br />
+        <button onClick={signIn as any}>Sign in</button>
+      </>}
+      {session && <>
+        Signed in as {session.user.name} <br />
+        <button onClick={signOut as any}>Sign out</button>
+      </>}
     </div>
   )
 }
