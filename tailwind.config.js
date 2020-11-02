2148efc0ee83fd7d97ecac46bdecf4666507fa07
Ngineer101 <nicolaasb@pm.me>
2020-11-02 11:30:58 +0100

    Added martel font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   },
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Martel', 'sans-serif'],
+      serif: ['Martel', 'sans-serif'],
+      mono: ['Martel', 'sans-serif'],
+      display: ['Martel', 'sans-serif'],
+      body: ['Martel', 'sans-serif'],
+    },
     extend: {
       colors: {
         'accent-1': '#333',
