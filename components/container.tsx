8fa4b1a6d4118eab7f01abf76d8916847609e222
Ngineer101 <nicolaasb@pm.me>
2020-11-29 21:04:13 +0100

    Added 'powered by' link in footer

--- components/container.tsx
+++ components/container.tsx
@@ -75,8 +75,12 @@ export default function PageContainer(props: {
       </main>
       {
         props.publicationName &&
-        <footer className='p-2 bg-black text-white flex justify-center'>
+        <footer className='p-2 bg-black text-white flex flex-col items-center justify-center'>
           <label>&#169; {new Date().getFullYear().toString()} - {props.publicationName}</label>
+          <small>Powered by <a className='text-white underline' target='_blank' href={`https://yappl.xyz?referrer=${props.publicationName}`}>
+            <strong>Yappl</strong>
+          </a>
+          </small>
         </footer>
       }
     </div>
