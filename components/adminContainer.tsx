29c1f5b30472d2ffd461748c21adc0cb39d9e794
Ngineer101 <nicolaasb@pm.me>
2020-12-11 19:48:40 +0100

    Styled navbar

--- components/adminContainer.tsx
+++ components/adminContainer.tsx
@@ -9,14 +9,26 @@ export default function AdminPageContainer(props: {
   const [session, loading] = useSession();
   return (
     <div className='flex flex-col justify-between w-full'>
-      <nav className='flex justify-end w-full p-2'>
+      <nav className='flex justify-between w-full p-2 items-center border-b-2 border-gray-300 shadow-md'>
         {
-          !session &&
+          !session && !loading &&
           <button className='btn-default' onClick={signIn as any}>Sign in</button>
         }
         {
-          session &&
+          session && !loading &&
           <>
+            <ul className='m-0 p-0'>
+              <li className='nav-item nav-item-border inline-block mr-2'>
+                <Link href="/dashboard">
+                  <a className='btn-nav'>Dashboard</a>
+                </Link>
+              </li>
+              <li className='nav-item nav-item-border inline-block'>
+                <Link href='/mail-settings'>
+                  <a className='btn-nav'>Mail settings</a>
+                </Link>
+              </li>
+            </ul>
             <div className='inline-flex'>
               <ul className='flex flex-wrap text-sm md:text-base'>
                 <li className="relative group rounded-lg">
@@ -30,12 +42,7 @@ export default function AdminPageContainer(props: {
                       </svg>
                   }
                   <ul className="absolute right-0 top-0 mt-12 p-2 rounded-lg shadow-lg hidden bg-white z-10 group-hover:block">
-                    <li className='nav-item'>
-                      <Link href="/dashboard">
-                        <a className='btn-nav text-gray-700 hover:text-black hover:bg-gray-300'>Dashboard</a>
-                      </Link>
-                    </li>
-                    <li className='nav-item'>
+                    <li className='nav-item nav-item-border'>
                       <button className='btn-nav' onClick={signOut as any}>Sign out</button>
                     </li>
                   </ul>
