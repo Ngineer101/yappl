73614dea311922af5a16862b4a405748cf9afbf8
Ngineer101 <nicolaasb@pm.me>
2020-12-11 20:19:22 +0100

    Hide signup input when emails are disabled

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Container from '../components/container'
 import axios from 'axios';
-import { Post, Publication } from '../models';
+import { MailProviders, MailSettings, Post, Publication } from '../models';
 import { useState } from 'react';
 import Link from 'next/link';
 import { emailRegex } from '../constants/emailRegex';
@@ -9,7 +9,10 @@ import { dbConnection } from '../repository';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 
-export default function Index(props: any) {
+export default function Index(props: {
+  publication: Publication,
+  mailActive: boolean,
+}) {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [loading, setLoading] = useState(false);
@@ -45,58 +48,61 @@ export default function Index(props: any) {
               <h1 className='text-center mb-2'>{publication.name}</h1>
               <h2 className='text-center mb-10 max-w-5xl'>{publication.description}</h2>
               <img src={publication.imageUrl ? publication.imageUrl : require('../public/assets/banner.svg')} className='h-64 max-w-full' />
-              <div className='max-w-2xl w-11/12 bg-cover bg-center mb-8 p-4'>
-                <form method='POST' onSubmit={(evt) => {
-                  evt.preventDefault();
-                  if (email && emailRegex.test(email) && !name) {
-                    setLoading(true);
-                    axios.post('/api/member/subscribe', {
-                      email,
-                      publicationId: publication.id
-                    })
-                      .then(response => {
-                        setEmail('');
-                        setLoading(false);
-                        setErrorMessage('');
-                        router.push('/thanks-for-subscribing');
+              {
+                props.mailActive &&
+                <div className='max-w-2xl w-11/12 bg-cover bg-center mb-8 p-4'>
+                  <form method='POST' onSubmit={(evt) => {
+                    evt.preventDefault();
+                    if (email && emailRegex.test(email) && !name) {
+                      setLoading(true);
+                      axios.post('/api/member/subscribe', {
+                        email,
+                        publicationId: publication.id
                       })
-                      .catch(error => {
-                        setLoading(false);
-                        if (error.response.data) {
-                          setErrorMessage(error.response.data);
-                        } else {
-                          setErrorMessage('An error occurred while subscribing.');
+                        .then(response => {
+                          setEmail('');
+                          setLoading(false);
+                          setErrorMessage('');
+                          router.push('/thanks-for-subscribing');
+                        })
+                        .catch(error => {
+                          setLoading(false);
+                          if (error.response.data) {
+                            setErrorMessage(error.response.data);
+                          } else {
+                            setErrorMessage('An error occurred while subscribing.');
+                          }
+                        });
+                    } else {
+                      setErrorMessage('Email is not valid.');
+                    }
+                  }}>
+                    <input name='sweet-field' className='sweet' placeholder='Sweet and sticky field' id='sweet' autoComplete='off' value={name}
+                      type='sticky-field' onChange={(evt) => setName(evt.currentTarget.value)} />
+                    <div className='relative shadow-2xl bg-gray-200'>
+                      <input type='email' placeholder='Enter your email to subscribe' autoFocus onChange={(evt) => setEmail(evt.currentTarget.value)}
+                        value={email} className='input-subscribe' />
+                      <button className='button-subscribe'
+                        type='submit' disabled={loading}>
+                        {
+                          loading &&
+                          <svg className="animate-spin h-5 w-5 m-3 rounded-full border-2" style={{ borderColor: 'white white black black' }} viewBox="0 0 24 24"></svg>
                         }
-                      });
-                  } else {
-                    setErrorMessage('Email is not valid.');
-                  }
-                }}>
-                  <input name='sweet-field' className='sweet' placeholder='Sweet and sticky field' id='sweet' autoComplete='off' value={name}
-                    type='sticky-field' onChange={(evt) => setName(evt.currentTarget.value)} />
-                  <div className='relative shadow-2xl bg-gray-200'>
-                    <input type='email' placeholder='Enter your email to subscribe' autoFocus onChange={(evt) => setEmail(evt.currentTarget.value)}
-                      value={email} className='input-subscribe' />
-                    <button className='button-subscribe'
-                      type='submit' disabled={loading}>
-                      {
-                        loading &&
-                        <svg className="animate-spin h-5 w-5 m-3 rounded-full border-2" style={{ borderColor: 'white white black black' }} viewBox="0 0 24 24"></svg>
-                      }
-                      {
-                        !loading &&
-                        <span>Subscribe</span>
-                      }
-                    </button>
-                  </div>
-                  {
-                    errorMessage &&
-                    <label className='text-red-500 mt-4 ml-2'>
-                      <strong>{errorMessage}</strong>
-                    </label>
-                  }
-                </form>
-              </div>
+                        {
+                          !loading &&
+                          <span>Subscribe</span>
+                        }
+                      </button>
+                    </div>
+                    {
+                      errorMessage &&
+                      <label className='text-red-500 mt-4 ml-2'>
+                        <strong>{errorMessage}</strong>
+                      </label>
+                    }
+                  </form>
+                </div>
+              }
               <div className='flex flex-col justify-center items-center my-8 adjusted-width'>
                 {
                   (publication.posts || []).map((p, i) =>
@@ -131,15 +137,18 @@ export default function Index(props: any) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (): Promise<any> => {
+export const getStaticProps: GetStaticProps = async (): Promise<any> => {
   const connection = await dbConnection('publication');
   const publicationRepository = connection.getRepository(Publication);
   let publication = await publicationRepository.findOne();
 
+  const mailSettingsRepository = connection.getRepository(MailSettings);
+  const mailSettings = await mailSettingsRepository.findOne();
   if (!publication) {
     await connection.close();
     return {
-      props: {}
+      props: {},
+      revalidate: 60,
     };
   } else {
     const postRepository = connection.getRepository(Post);
@@ -149,8 +158,10 @@ export const getServerSideProps: GetServerSideProps = async (): Promise<any> => 
 
     return {
       props: {
+        mailActive: mailSettings && mailSettings.provider !== MailProviders.NONE,
         publication: JSON.parse(JSON.stringify(publication))
-      }
+      },
+      revalidate: 60,
     }
   }
 }
